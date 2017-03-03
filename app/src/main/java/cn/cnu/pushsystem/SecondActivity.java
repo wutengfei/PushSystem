fe07ede0addb0d31bf1dd8b827fcbf311fbc11f1
wutengfei <781862614@qq.com>
2017-03-03 14:23:40 +0800

    添加几行注释

--- app/src/main/java/cn/cnu/pushsystem/SecondActivity.java
+++ app/src/main/java/cn/cnu/pushsystem/SecondActivity.java
@@ -6,23 +6,24 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-TextView textView;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        textView= (TextView) findViewById(R.id.textView);
-
+        textView = (TextView) findViewById(R.id.textView);
+       //从DemoMessageReceiver传来的Intent
         Intent intent = getIntent();
         String content = intent.getStringExtra("content");
-        String message=intent.getStringExtra("message");
-        content=1+"apple";
-       // 取字符串的前i个字符
-              String  str=content.substring(0,1);
-       //去掉字符串的前i个字符：
-        String str2=content.substring(1);
-        System.out.println(str+"---------------");
-        System.out.println(str2+"---------------");
-        textView.setText(content+"---"+message);
+        String message = intent.getStringExtra("message");
+        content = 1 + "apple";
+        // 取字符串的前i个字符
+        String str = content.substring(0, 1);
+        //去掉字符串的前i个字符：
+        String str2 = content.substring(1);
+        System.out.println(str + "---------------");
+        System.out.println(str2 + "---------------");
+        textView.setText(content + "---" + message);
     }
 }
